0ea4184922d8c5f42134c48d1b4506ae16bb0d84
Am-iir <maharjanamir.101@gmail.com>
2025-01-13 10:04:15 +1100

    Hanlded form data

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.use(express.json());
+app.use(express.json()); //handles json
+app.use(express.urlencoded({extended:false})); //handles form data
 
 // Using Node.js `require()`
 const mongoose = require('mongoose');
