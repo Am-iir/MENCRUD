9ab59d2d1983846218ff6d720583e73043ae8105
Am-iir <maharjanamir.101@gmail.com>
2025-01-13 09:33:32 +1100

    Update API added

--- index.js
+++ index.js
@@ -62,3 +62,27 @@ app.get('/api/products/:id',async(req,res) => {
   
     }
 });
+
+// Update Product
+
+app.put('/api/products/:id', async(req,res) => {
+ try {
+    const {id} = req.params;
+
+   const product = await Product.findByIdAndUpdate(id,req.body);
+    if(!product){
+        return res.status(404).json({message:"Product not found"});
+
+    }
+
+    const updatedProduct = await Product.findById(id)
+
+    res.status(200).json(updatedProduct);
+    
+
+} catch (error) {
+
+    res.status(500).json({message:error.message});
+    
+ }
+} );
