cc7a6a07614bbb710ef6f22c2807e7303bd171c3
Am-iir <maharjanamir.101@gmail.com>
2025-01-13 23:43:53 +1100

    Beautified to routes and controller

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const app = express()
 app.use(express.json()); //handles json
 app.use(express.urlencoded({extended:false})); //handles form data
 
-// Using Node.js `require()`
+// middleware
 const mongoose = require('mongoose');
 const Product =  require('./models/product.model');
-
+const productRoute = require('./routes/product.route.js')
+//routes
+app.use("/api/products",productRoute);
 
 mongoose.connect("mongodb+srv://maharjanamir101:123456qwerty@backenddb.k4pdo.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
 .then(() => {
@@ -27,42 +29,7 @@ app.get('/', (req, res) => {
     res.send("Hello From Node API Server Updated");
 })
 
-app.post('/api/products', async(req,res) => {
-   
-    try{
 
-        const product = await Product.create(req.body);
-        res.status(200).json(product);
-
-    }catch(error){
-        res.status(500).json({message:error.message});
-    }
-});
-
-app.get('/api/products', async(req,res) => {
-   
-    try{
-
-        const products = await Product.find({});
-        res.status(200).json(products);
-
-    }catch(error){
-        res.status(500).json({message:error.message});
-    }
-});
-
-app.get('/api/products/:id',async(req,res) => {
-
-    try{
-        const {id} = req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product);
-
-    }catch(error){
-        res.status(500).json({message:error.message});
-  
-    }
-});
 
 // Update Product
 
